8412bec5061ced45152e6234cdf7e9b8dbd0d820
Tyler Roemer <troemer@mail.greenriver.edu>
2018-01-16 21:42:00 -0500

    created sort and print function

--- array-review.php
+++ array-review.php
@@ -1,3 +1,11 @@
 <?php
+    //part 1
+    echo '<h1>Part 1</h1>';
 
+    function SParray($array){
+        sort($array);
+        foreach ($array as $value)
+            echo "$value ";
+        echo '<br>';
+    }
 ?>
