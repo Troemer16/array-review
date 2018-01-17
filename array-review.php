8d6ef6d35e64fc416eb6daddd0646a1ac3384221
Tyler Roemer <troemer@mail.greenriver.edu>
2018-01-16 21:44:08 -0500

    finished part 1

--- array-review.php
+++ array-review.php
@@ -8,4 +8,25 @@
             echo "$value ";
         echo '<br>';
     }
+
+    function arrayAdd($string, &$array){
+        echo "adding $string..";
+
+        if(!in_array(strtolower($string), array_map('strtolower', $array)))
+            array_push($array, $string);
+
+        echo '<br>';
+    }
+
+    $animals = array('panda', 'alpaca', 'boa');
+
+    SParray($animals);
+    arrayAdd('goat', $animals);
+    SParray($animals);
+    arrayAdd('Boa', $animals);
+    SParray($animals);
+
+    echo '<br>';
+    echo '<br>';
+
 ?>
